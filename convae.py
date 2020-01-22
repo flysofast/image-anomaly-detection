41f8da8e0fc5886da2c82f9d3240156e2908114d
Nam Le <le4@narvi.cc.tut.fi>
2020-01-22 23:33:20 +0200

    Change saving folders for image and weights files

--- convae.py
+++ convae.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision.utils import save_image
+import os
 
 # Loading and Transforming data
 transform = transforms.Compose([
@@ -13,9 +14,8 @@ transform = transforms.Compose([
             ])
 trainTransform  = tv.transforms.Compose([
                     tv.transforms.ToTensor(), 
-                    tv.transforms.Normalize((0.4914, 0.4822, 0.4466), (0.247, 0.243, 0.261))
+                    # tv.transforms.Normalize((0.4914, 0.4822, 0.4466), (0.247, 0.243, 0.261))
                 ])
-trainset = tv.datasets.CIFAR10(root='./data',  train=True,download=True, transform=transform)
 
 
 # Writing our model
@@ -24,14 +24,14 @@ class Autoencoder(nn.Module):
         super(Autoencoder,self).__init__()
         
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 6, kernel_size=5),
+            nn.Conv2d(3, 16, kernel_size=3),
             nn.ReLU(True),
-            nn.Conv2d(6,16,kernel_size=5),
+            nn.Conv2d(16,6,kernel_size=3),
             nn.ReLU(True))
         self.decoder = nn.Sequential(             
-            nn.ConvTranspose2d(16,6,kernel_size=5),
+            nn.ConvTranspose2d(6,16,kernel_size=3),
             nn.ReLU(True),
-            nn.ConvTranspose2d(6,3,kernel_size=5),
+            nn.ConvTranspose2d(16,3,kernel_size=3),
             nn.ReLU(True))
     def forward(self,x):
         x = self.encoder(x)
@@ -39,12 +39,14 @@ class Autoencoder(nn.Module):
         return x
 
 #defining some params
-num_epochs = 5 
-batch_size = 32
-dataloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=False, num_workers=4)
+num_epochs = 200 
+batch_size = 128
+
+trainset = tv.datasets.CIFAR10(root='./data',  train=True,download=True, transform=transform)
+dataloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=4)
 testset = tv.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-testloader = torch.utils.data.DataLoader(testset, batch_size=2, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=True, num_workers=2)
 
 model = Autoencoder()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -55,7 +57,11 @@ model = model.to(device)
 
 distance = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(),weight_decay=1e-5)
+output_folder = "output"
+weights_folder = os.path.join(output_folder, "weights")
+image_folder = os.path.join(output_folder, "image")
 
+os.makedirs(weights_folder, exist_ok = True)
 for epoch in range(num_epochs):
     for data in dataloader:
         img, _ = data
@@ -69,4 +75,18 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
     # ===================log========================
-    print('epoch [{}/{}], loss:{:.4f}'.format(epoch+1, num_epochs, loss.data()))
+    print('epoch [{}/{}], loss:{:.4f}'.format(epoch+1, num_epochs, loss.data.cpu().numpy()))
+    if isinstance(model, nn.DataParallel):
+        state_dict = model.module.state_dict()
+    else:
+        state_dict = mode.state_dict()
+    torch.save(state_dict, os.path.join(weights_folder, f"model_{epoch}.pth"))
+    for data in testloader:
+        img, _ = data
+        img=img.to(device)
+        output = model(img)
+        output = output.detach()
+        image = torch.cat((img, output), 2)
+        image = image.cpu()
+        save_image(image,os.path.join(image_folder, f"output_{epoch}.png") )
+
