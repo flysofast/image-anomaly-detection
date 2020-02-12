0d3e1656a2feb22d42619c258d1c3772c9fb3ecf
Nam Le <hai.le@student.tut.fi>
2020-02-12 22:29:14 +0200

    Save best epochs

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ val_loader = DataLoader(valset, batch_size=1, num_workers=4)
 
 num_epochs = 200 
 batch_size = 32
-
+save_interval = 10
 trainset = MVTecAd(subset="train", category="hazelnut", root_dir="dataset/mvtec_anomaly_detection", transform=train_data_transform)
 train_loader = DataLoader(trainset, batch_size=batch_size, num_workers=4)
 
@@ -58,11 +58,12 @@ os.makedirs(results_folder, exist_ok = True)
 writer = SummaryWriter(log_dir=f"log/{exp_name}")
 
 print("Start training")
+min_loss = 1e10
 for epoch in range(num_epochs):
     model.train(True)
     pbar = tqdm(train_loader)
     pbar.set_description(f"Epoch {epoch+1}/{num_epochs}")
-
+    epoch_loss = 0
     for i, img in enumerate(pbar):
         img = img.to(device)
         optimizer.zero_grad()
@@ -71,20 +72,24 @@ for epoch in range(num_epochs):
 
         loss.backward()
         optimizer.step()
+        loss_val = loss.item()
+        epoch_loss += loss_val
+        writer.add_scalar("MSE/train", loss_val, epoch * len(train_loader) + i)
 
-        writer.add_scalar("MSE/train", loss.item(), epoch * len(train_loader) + i)
 
+    print('epoch [{}/{}], loss:{:.4f}'.format(epoch+1, num_epochs, epoch_loss/len(train_loader)))
 
-    print('epoch [{}/{}], loss:{:.4f}'.format(epoch+1, num_epochs, loss.data.cpu().numpy()))
-    if isinstance(model, nn.DataParallel):
-        state_dict = model.module.state_dict()
-    else:
-        state_dict = model.state_dict()
-    model_file = os.path.join(weights_folder, f"model_{epoch}.pth")
-    torch.save(state_dict, model_file)
-    print(f"Model saved at {model_file}")
+    if epoch_loss < min_loss or epoch % save_interval == 0:
+        
+        if isinstance(model, nn.DataParallel):
+            state_dict = model.module.state_dict()
+        else:
+            state_dict = model.state_dict()
+        model_file = os.path.join(weights_folder, f"model_{epoch}.pth")
+        torch.save(state_dict, model_file)
+        print(f"Model saved at {model_file}")
 
-    print("Validating")
+    print("Validating...")
     model.eval()
     with torch.no_grad():
         for index, img in enumerate(tqdm(val_loader)):
@@ -92,21 +97,26 @@ for epoch in range(num_epochs):
             img = img.to(device)
             output = model(img)
             loss = loss_op(output, img).data.cpu()
-            diff = torch.abs(img - output)
-            # breakpoint()
-            # Repeat one channeled mean values along channel axis to the same shape of an image
-            diff_avg = torch.mean(diff, dim=1).unsqueeze(1).expand(-1, 3, -1, -1)
-            # correct = correct + 1 if loss.item() > 0.5
-            image = torch.cat((img, output, diff, diff_avg), 0)
-            image = image.cpu()
-            result_image = os.path.join(results_folder, f"output_{epoch}_{loss:.4f}.png")
-            # save_image(image, result_image)
-            # create grid of images
-            img_grid = torchvision.utils.make_grid(image)
-            torchvision.utils.save_image(image, result_image, nrow=image.shape[0])
+            
+            if epoch_loss < min_loss or epoch % save_interval == 0:
+                diff = torch.abs(img - output)
+                
+                # Repeat one channeled mean values along channel axis to the same shape of an image
+                diff_avg = torch.mean(diff, dim=1).unsqueeze(1).expand(-1, 3, -1, -1)
+                
+                image = torch.cat((img, output, diff, diff_avg), 0)
+                image = image.cpu()
+                result_image = os.path.join(results_folder, f"output_{epoch}_{loss:.4f}.png")
+
+                # create grid of images
+                img_grid = torchvision.utils.make_grid(image)
+                torchvision.utils.save_image(image, result_image, nrow=image.shape[0])
+                print(f"Result images saved at {results_folder}")
 
             # write to tensorboard
             writer.add_scalar("MSE/val", loss.item(), global_step=epoch)
             writer.add_image('Val images', img_grid, global_step=epoch)
-        print(f"Result images saved at {results_folder}")
+    
+    if epoch_loss < min_loss:
+        min_loss = epoch_loss
 
