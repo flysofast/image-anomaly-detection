438609bab494a93eb361f7857a916136e04be0b8
Nam Le <hai.le@student.tut.fi>
2020-02-18 23:36:27 +0200

    fix gpu training bug

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CUDA_VISIBILE_DEVICES=0,1 python main.py --batch_size 32 --exp_name BottleNeck_3 --seed 42 --crop_size 300 --category hazelnut
+CUDA_VISIBILE_DEVICES=0,1 python main.py --batch_size 32 --exp_name BottleNeck_4 --seed 42 --crop_size 300 --category hazelnut
