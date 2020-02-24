06443a578f53408d16b5d89983491de90319757d
Nam Le <hai.le@student.tut.fi>
2020-02-24 21:24:03 +0200

    zipper training

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CUDA_VISIBILE_DEVICES=0,1 python main.py --batch_size 32 --exp_name BottleNeck_4 --seed 42 --crop_size 300 --category hazelnut
+CUDA_VISIBILE_DEVICES=0,1 python main.py --batch_size 32 --exp_name BottleNeck_5 --seed 42 --crop_size 128 --category zipper
